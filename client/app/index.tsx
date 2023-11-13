c5c93ccd83fa5a1d7dcb474d0629578dd7de5eed
Adam Jałocha <energywraith@gmail.com>
2023-11-13 14:12:16 +0100

    Removed screens directory

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,5 +1,26 @@
-import MainScreen from "../screens/MainScreen";
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 
-export default function Main() {
-  return <MainScreen />;
+import AboutUs from "../components/templates/AboutUs";
+import Gallery from "../components/templates/Gallery";
+import Services from "../components/templates/Services";
+import Offer from "../components/templates/Offer";
+
+export default function MainScreen() {
+  const onPressLearnMore = () => {
+    router.replace("/posts");
+  };
+
+  return (
+    <ScrollView>
+      <SafeAreaView>
+        <AboutUs onPressLearnMore={onPressLearnMore} />
+        <Gallery />
+        <Services />
+        <Offer onPressLearnMore={onPressLearnMore} />
+      </SafeAreaView>
+    </ScrollView>
+  );
 }
