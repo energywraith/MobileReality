567e4221e28065a0a559b7808cc579e0cc3174b7
Adam Jałocha <energywraith@gmail.com>
2023-11-13 13:26:54 +0100

    Made InfoSection Component from AboutUs to use it in Offer

--- client/components/templates/AboutUs.tsx
+++ client/components/templates/AboutUs.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { GestureResponderEvent, StyleSheet } from "react-native";
+import { GestureResponderEvent } from "react-native";
 
-import { Text, View } from "../Themed";
-import Button from "../common/Button";
+import InfoSection from "./InfoSection";
 
 interface AboutUsProps {
   onPressLearnMore: (event: GestureResponderEvent) => void;
@@ -10,73 +9,12 @@ interface AboutUsProps {
 
 export default function AboutUs({ onPressLearnMore }: AboutUsProps) {
   return (
-    <View style={styles.container}>
-      <Text
-        style={styles.header}
-        lightColor="rgb(0,0,0,0)"
-        darkColor="rgba(255,255,255,0.8)"
-      >
-        KILKA SŁÓW O NAS
-      </Text>
-
-      <Text
-        style={styles.subHeader}
-        lightColor="rgba(2, 129, 192, 0.8)"
-        darkColor="rgba(2, 129, 192, 0.8)"
-      >
-        CZYLI KIM JESTEŚMY I DOKAD ZMIERZAMY
-      </Text>
-
-      <Text
-        style={styles.paragraph}
-        lightColor="rgba(0,0,0,0.6)"
-        darkColor="rgba(255,255,255,0.8)"
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio iusto
-        excepturi necessitatibus omnis illo dignissimos fugiat ipsa vero ipsum,
-        enim doloremque nostrum nihil? Est voluptatum expedita modi voluptatem
-        magnam sapiente eius ipsam officia, repellat, alias harum libero,
-        reiciendis adipisci iste aspernatur deleniti ex iusto dignissimos
-        aliquid incidunt. Magnam, perferendis aliquid? {"\n"} {"\n"}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eveniet
-        consectetur, ipsam labore ullam dolor nesciunt officia ipsa saepe illum!
-        Omnis dolor non numquam sequi?
-      </Text>
-
-      <Button
-        onPress={onPressLearnMore}
-        title="ZOBACZ WIECEJ"
-        style={styles.button}
-      />
-    </View>
+    <InfoSection
+      header="KILKA SŁÓW O NAS"
+      subHeader="CZYLI KIM JESTEŚMY I DOKAD ZMIERZAMY"
+      paragraph={`Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio iusto excepturi necessitatibus omnis illo dignissimos fugiat ipsa vero ipsum, enim doloremque nostrum nihil? Est voluptatum expedita modi voluptatem magnam sapiente eius ipsam officia, repellat, alias harum libero, reiciendis adipisci iste aspernatur deleniti ex iusto dignissimos aliquid incidunt. Magnam, perferendis aliquid?\n\n Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eveniet consectetur, ipsam labore ullam dolor nesciunt officia ipsa saepe illum! Omnis dolor non numquam sequi?`}
+      buttonTitle="ZOBACZ WIECEJ"
+      onPressLearnMore={onPressLearnMore}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-    paddingVertical: 64,
-  },
-  header: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-  subHeader: {
-    fontSize: 14,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 24,
-  },
-  paragraph: {
-    fontSize: 14,
-    lineHeight: 24,
-    textAlign: "center",
-  },
-  button: {
-    marginTop: 24,
-  },
-});
---
+++ client/components/templates/InfoSection.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { GestureResponderEvent, StyleSheet } from "react-native";
+
+import { Text, View } from "../Themed";
+import Button from "../common/Button";
+
+interface InfoSectionProps {
+  header: string;
+  subHeader: string;
+  paragraph: string;
+  buttonTitle: string;
+  onPressLearnMore: (event: GestureResponderEvent) => void;
+}
+
+export default function InfoSection({
+  header,
+  subHeader,
+  paragraph,
+  buttonTitle,
+  onPressLearnMore,
+}: InfoSectionProps) {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={styles.header}
+        lightColor="rgb(0,0,0,0)"
+        darkColor="rgba(255,255,255,0.8)"
+      >
+        {header}
+      </Text>
+
+      <Text
+        style={styles.subHeader}
+        lightColor="rgba(2, 129, 192, 0.8)"
+        darkColor="rgba(2, 129, 192, 0.8)"
+      >
+        {subHeader}
+      </Text>
+
+      <Text
+        style={styles.paragraph}
+        lightColor="rgba(0,0,0,0.6)"
+        darkColor="rgba(255,255,255,0.8)"
+      >
+        {paragraph}
+      </Text>
+
+      <Button
+        onPress={onPressLearnMore}
+        title={buttonTitle}
+        style={styles.button}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    paddingVertical: 64,
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  subHeader: {
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  paragraph: {
+    fontSize: 14,
+    lineHeight: 24,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 24,
+  },
+});
