c2cd686b012be481df087a585e017f153f8b5065
Adam Jałocha <energywraith@gmail.com>
2023-11-13 13:27:03 +0100

    Added Offer Component with use of InfoSection

---
+++ client/components/templates/Offer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { GestureResponderEvent } from "react-native";
+
+import InfoSection from "./InfoSection";
+
+interface AboutUsProps {
+  onPressLearnMore: (event: GestureResponderEvent) => void;
+}
+
+export default function Offer({ onPressLearnMore }: AboutUsProps) {
+  return (
+    <InfoSection
+      header="NASZA OFERTA"
+      subHeader="DOWIEDZ SIE CO MOZEMY TOBIE ZAOFEROWAC"
+      paragraph={`Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio iusto excepturi necessitatibus omnis illo dignissimos fugiat ipsa vero ipsum, enim doloremque nostrum nihil? Est voluptatum expedita modi voluptatem magnam sapiente eius ipsam officia, repellat, alias harum libero, reiciendis adipisci iste aspernatur deleniti ex iusto dignissimos aliquid incidunt. Magnam, perferendis aliquid?\n\n Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eveniet consectetur, ipsam labore ullam dolor nesciunt officia ipsa saepe illum! Omnis dolor non numquam sequi?`}
+      buttonTitle="ZOBACZ WIECEJ"
+      onPressLearnMore={onPressLearnMore}
+    />
+  );
+}
--- client/screens/MainScreen.tsx
+++ client/screens/MainScreen.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import AboutUs from "../components/templates/AboutUs";
 import Gallery from "../components/templates/Gallery";
 import Services from "../components/templates/Services";
+import Offer from "../components/templates/Offer";
 
 export default function MainScreen() {
   const onPressLearnMore = () => {};
@@ -12,6 +13,7 @@ export default function MainScreen() {
       <AboutUs onPressLearnMore={onPressLearnMore} />
       <Gallery />
       <Services />
+      <Offer onPressLearnMore={onPressLearnMore} />
     </>
   );
 }
