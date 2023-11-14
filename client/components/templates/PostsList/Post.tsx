0d3324ad8adb4953eb402bf444cc7d157051b9e0
Adam Jałocha <energywraith@gmail.com>
2023-11-14 15:07:22 +0100

    Restyled PostsList, Added Details, Remove buttons

--- client/components/templates/PostsList/Post.tsx
+++ client/components/templates/PostsList/Post.tsx
@@ -1,31 +1,55 @@
 import { Text } from "../../Themed";
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { Post } from "../../../features/posts/postsSlice";
+import Button from "../../common/Button";
 
-interface PostProps {
-  id: Post["id"];
-  title: Post["title"];
+interface PostProps extends Omit<Post, "createdAt"> {
   onPressPost: (id: Post["id"]) => void;
+  onPressPostDelete: (id: Post["id"]) => void;
 }
 
-export default function PostComponent({ id, title, onPressPost }: PostProps) {
+export default function PostComponent({
+  id,
+  title,
+  description,
+  onPressPost,
+  onPressPostDelete,
+}: PostProps) {
   return (
-    <Pressable onPress={() => onPressPost(id)}>
-      <Text style={styles.post}>{title}</Text>
+    <Pressable onPress={() => onPressPost(id)} style={styles.post}>
+      <Text style={styles.postTitle}>{title}</Text>
+      <Text style={styles.postDescription} numberOfLines={2}>
+        {description}
+      </Text>
+      <View style={styles.buttons}>
+        <Button title="Details" onPress={() => onPressPost(id)} />
+        <Button title="Remove" onPress={() => onPressPostDelete(id)} />
+      </View>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   post: {
-    color: "white",
+    color: "black",
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+    paddingBottom: 32,
+    display: "flex",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.3)",
+  },
+  postTitle: {
     fontSize: 24,
     lineHeight: 48,
     fontWeight: "bold",
-    paddingHorizontal: 24,
-    backgroundColor: "#000000c0",
+  },
+  postDescription: {},
+  buttons: {
     display: "flex",
     flexDirection: "row",
-    justifyContent: "space-between",
+    marginTop: 16,
+    columnGap: 8,
   },
 });
--- client/components/templates/PostsList/index.tsx
+++ client/components/templates/PostsList/index.tsx
@@ -3,14 +3,26 @@ import { useAppSelector } from "../../../hooks/useAppSelector";
 import { router } from "expo-router";
 import { Post as IPost } from "../../../features/posts/postsSlice";
 import Post from "./Post";
+import { useAppDispatch } from "../../../hooks/useAppDispatch";
+import { deletePost } from "../../../features/posts/postsThunks";
 
 export default function PostsList() {
   const { value: posts, isLoading: isPostsLoading } = useAppSelector(
     (state) => state.posts
   );
+  const dispatch = useAppDispatch();
 
   const onPressPostDetails = (id: IPost["id"]) => {
     router.replace(`/posts/${id}`);
+  };
+
+  const onPressPostDelete = async (id: IPost["id"]) => {
+    try {
+      await dispatch(deletePost(id.toString()));
+      router.replace("/posts");
+    } catch (error) {
+      throw error;
+    }
   };
 
   if (isPostsLoading) {
@@ -22,7 +34,9 @@ export default function PostsList() {
       key={post.id}
       id={post.id}
       title={post.title}
+      description={post.description}
       onPressPost={onPressPostDetails}
+      onPressPostDelete={onPressPostDelete}
     />
   ));
 }
